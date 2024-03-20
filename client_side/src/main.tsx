206038a7f254aa0db4598da576525e8de30994e9
BrayanDurazo <brayan9612@gmail.com>
2024-03-19 23:15:18 -0700

    add apollo and graphql

--- client_side/src/main.tsx
+++ client_side/src/main.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { RestLink } from 'apollo-link-rest';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// Set `RestLink` with your endpoint
+const restLink = new RestLink({ 
+  uri: 'http://127.0.0.1:8000/',
+  endpoints: { v1: 'http://127.0.0.1:8000/api/v1/todo/' }
+});
+
+// Setup Apollo client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: restLink,
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ApolloProvider>,
 );
