b8cc3a7621acd79449adcef94746ebefb2f903c8
BrayanDurazo <brayan9612@gmail.com>
2024-05-14 23:19:45 -0700

    update item model to create empty items
    Since the approach will now be to create the
    items empty, then update their description or
    checked status if given, had to update the item
    model to be able to create empty items.

--- server_side/todo/models.py
+++ server_side/todo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Item(models.Model):
     checked = models.BooleanField()
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.description
